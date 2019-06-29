c3ac75effea782ba569d453dc15c5d75b2a0406d
Mateusz Brawański <emzi0767@gmail.com>
2019-06-30 00:41:05 +0200

    Moved source into source directory.

---
+++ src/SlimGet/Controllers/GalleryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SlimGet.Controllers
+{
+    [AllowAnonymous]
+    public class GalleryController : Controller
+    {
+        [Route("/v3/index.json"), HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/SlimGet/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using SlimGet.Data.Configuration;
+using SlimGet.Models;
+using SlimGet.Services;
+
+namespace SlimGet
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            var cfg = new ConfigurationBuilder()
+                .AddConfiguration(configuration)
+                .AddJsonFile("slimget.json", false)
+                .Build();
+
+            this.Configuration = cfg;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .Configure<StorageConfiguration>(opts => this.Configuration.GetSection("Storage").Bind(opts))
+                .Configure<ServerConfiguration>(opts => this.Configuration.GetSection("Server").Bind(opts));
+
+            services.AddHsts(opts =>
+            {
+                opts.Preload = true;
+                opts.IncludeSubDomains = true;
+                opts.MaxAge = TimeSpan.FromDays(365);
+            });
+
+            services.AddSingleton<ConnectionStringProvider>()
+                .AddDbContext<SlimGetContext>(ServiceLifetime.Transient)
+                .AddSingleton<RedisService>();
+
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseExceptionHandler("/error")
+                .UseStatusCodePages(this.RenderStatusCodeAsync)
+                .UseStaticFiles()
+                .UseMvc(routes => { });
+
+            if (!env.IsDevelopment())
+                app.UseHsts();
+
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            using (var db = scope.ServiceProvider.GetRequiredService<SlimGetContext>())
+                db.Database.Migrate();
+        }
+
+        private async Task RenderStatusCodeAsync(StatusCodeContext ctx)
+        {
+            var json = JsonConvert.SerializeObject(new SimpleErrorModel(Activity.Current?.Id ?? ctx.HttpContext.TraceIdentifier), Formatting.Indented);
+
+            ctx.HttpContext.Response.ContentType = "application/json";
+            await ctx.HttpContext.Response.WriteAsync(json, Utilities.UTF8);
+        }
+    }
+}
