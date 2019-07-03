82f27a433d3b96f3769a6c7b0bda083ce8bc5198
Mateusz Brawański <emzi0767@gmail.com>
2019-07-03 19:16:41 +0200

    Append license headers.

--- src/SlimGet.Database/addmigration.bat
+++ src/SlimGet.Database/addmigration.bat
@@ -1,4 +1,20 @@
 @echo off
 
+rem This file is a part of SlimGet project.
+rem
+rem Copyright 2019 Emzi0767
+rem 
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem 
+rem   http:remwww.apache.org/licenses/LICENSE-2.0
+rem 
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+
 dotnet ef migrations add --startup-project ..\SlimGet -o Migrations "%*"
 dotnet ef migrations script --startup-project ..\SlimGet
